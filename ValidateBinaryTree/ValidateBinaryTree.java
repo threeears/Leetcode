050e1541d0c63634b2c6b2bcb93c8cc4ba6baa74
threeears <zichennie@gmail.com>
2015-01-23 23:40:24 -0800

    BST

--- ValidateBinaryTree/ValidateBinaryTree.java
+++ ValidateBinaryTree/ValidateBinaryTree.java
@@ -16,45 +16,77 @@
  * }
  */
 public class Solution {
-    public boolean isValidBST(TreeNode root) {
-
-    	if(root==null) return true;
-
-    	if(root!=null && root.left==null && root.right==null) return true;
-
-    	if(root.left!=null)
-    	{// find out the minimnum element in subleftTree
-    		if(findMax(root.left)<root.val && isValidBST(root.left)) return true;
-
-       	}
-
-       	if(root.right!=null)
-       	{// find out the maximun element in subRightTree
-       		if(findMin(root.right) > root.val && isValidBST(root.right)) return true;
-       	}
-
-       	return false;
+  public boolean isValidBST(TreeNode root) {
+    // correct but not efficient
+        if(root==null) return true;     
+        boolean checkLeft = false;
+        boolean checkRight = false;
+        if(root.left!=null && findMax(root.left)<root.val && isValidBST(root.left) || root.left==null){
+            checkLeft = true;
+        }
+        if(root.right!=null && findMin(root.right)>root.val && isValidBST(root.right) || root.right==null){
+            checkRight = true;
+        }
+        
+        return checkLeft & checkRight;
 
     }
     public int findMin(TreeNode root){
-    	if(root.left==null && root.right!=null)
-    		return findMin(root.right);
-    	if(root.right==null && root.left!=null)
-    		return findMin(root.left);
-    	if(root.left!=null && root.right!=null)
-    		return Math.min(findMin(root.right), findMin(root.left));
-    	if(root.left==null && root.right==null)
-    		return root.val;
-
+        if(root!=null){
+            int result = root.val;
+            int min1 = findMin(root.left);
+            int min2 = findMin(root.right); 
+            min1=Math.min(min1,min2);
+            return Math.min(min1,result);
+        }
+        else return Integer.MAX_VALUE;
     }
     public int findMax(TreeNode root){
-    	if(root.left==null && root.right!=null)
-    		return findMax(root.right);
-    	if(root.right==null && root.left!=null)
-    		return findMax(root.left);
-    	if(root.left!=null && root.right!=null)
-    		return Math.max(findMin(root.right), findMin(root.left));
-    	if(root.left==null && root.right==null)
-    		return root.val;
+        if(root!=null){
+           int result = root.val;
+            int max1 = findMax(root.left);
+            int max2 = findMax(root.right);
+            max1 = Math.max(max1,max2);
+            return Math.max(max1,result);
+        }
+        else return Integer.MIN_VALUE;
+    }
+
+    //more efficient one
+    // for left tree, we only care if all the  nodes are smaller than certain value
+    // for right tree, we  onlu care if all the nodes are larger than certain value
+    // take care of overflaw
+    public boolean isValidBST(TreeNode root) {
+      if(root==null)
+        return true;
+      return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    public boolean helper(TreeNode root, long min, long max){
+        if(root==null) return true;
+        if(root.val < min || root.val > max){
+            return false;
+        }
+        return helper(root.left, min, (long)root.val-1) && helper(root.right, (long)root.val+1, max);//ALLOW ONLY DISTINCT VALUES
+    }
+
+    // from nineChapter, do not understand yet
+    private int lastVal = Integer.MIN_VALUE;
+    private boolean firstNode = true;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (!firstNode && lastVal >= root.val) {
+            return false;
+        }
+        firstNode = false;
+        lastVal = root.val;
+        if (!isValidBST(root.right)) {
+            return false;
+        }
+        return true;
     }
 }
