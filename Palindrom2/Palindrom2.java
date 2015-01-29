7af8d8844b22fba5c190667bbfd61dec389ff601
threeears <zichennie@gmail.com>
2015-01-28 17:22:51 -0800

    Palindrome

--- Palindrom2/Palindrom2.java
+++ Palindrom2/Palindrom2.java
@@ -1,53 +1,36 @@
 public class Solution {
-    public int minCut(String s) {
-
-    	int size  = s.length();
-    	boolean isPalindrom[][] = new boolean[size][size];
-    	int minCut[][] = new int[size][size];
-
-		if(checkPalindrom(0, size-1, s, isPalindrom)==true)
-        return 0;
-
-    	/*initialize*/
-    	for(int i = 0; i < size; i++)
-    		for(int j = 0; j < size; j++)
-    			if(i==j || checkPalindrom(i,j,s,isPalindrom)==true)
-    				minCut[i][j] = 0;
-    			else minCut[i][j] = size + 1;//give a max interger which cannot be the number of minCut
-
-    	/*compute minCut for every string*/
-    	return computeMinCut(0, size-1, s, isPalindrom,minCut);
-    	
-    	}
-
-    	
-   public int computeMinCut(int i, int j, String s, boolean[][] checkPalindrom, int[][] minCut){//compute mincut from i to j
-		if(minCut[i][j]==0) return 0;
-		for(int pointer=0; pointer < s.length(); pointer++){
-		 	if(checkPalindrom[i][pointer]==true){
-		      minCut[i][j] = Math.min(1+computeMinCut(pointer+1,j,s,checkPalindrom,minCut),minCut[i][j]);
-		    	}   
-		 }
-
-		return minCut[i][j];
-	}
-
-    public boolean checkPalindrom(int i, int j, String s, boolean[][] sP){
-    	boolean temp = false;
-
-    	if(i == j)  temp = true;
-    	
-    	else if((j-i+1==2) && (s.charAt(i)==s.charAt(j))) temp = true;
-
-    	else if((j-i+1 >2) && (s.charAt(i)==s.charAt(j)) && (checkPalindrom(i+1,j-1,s,sP)==true)) temp = true;
-
-    	sP[i][j] = temp;
-    	return temp;
-
+	// my answer also O(n^2) O(n^2)
+   public int minCut(String s) {
+        boolean[][] pal = new boolean[s.length()][s.length()];
+        int[] cut = new int[s.length()+1];//the smallest number of palindromes of length i
+        isPalindrome(s,pal);
+        cut[0]=0;// string is empty
+        for(int i = 1;i<=s.length();i++){
+            cut[i]=i;//num of palindromes
+            for(int j =i;j>=1;j--){
+                if(pal[j-1][i-1]==true){
+                    cut[i] = Math.min(cut[j-1]+1,cut[i]);
+                }
+            }
+        }
+        return cut[s.length()]-1;
+    }
+      private void isPalindrome(String s,boolean[][] pal) {
+      	//O(n^2)
+      for(int i = 0;i<s.length();i++){
+          for(int j=0;j<=i;j++){
+              if(i==j) 
+                    pal[i][j]=true;
+              else if(i-j+1<=3) 
+                    pal[j][i]=s.charAt(j)==s.charAt(i)?true:false;// distance is 2 or 3
+              else
+                    pal[j][i]=pal[j+1][i-1] && s.charAt(j)==s.charAt(i);
+            }
+      }
     }
 
-// final answer!! O(n^2) O(n^2)
-     public int minCut3 (String s){
+//  O(n^2) O(n^2), just combine my two steps together, check palindrome and find cut at the same time.
+     public int minCut2 (String s){
 	    	int[] distance=new int[s.length()];
 	    	for(int i = 0;i< s.length();i++){
 	    		distance[i]=s.length()-i-1;//max cut for each string from i to end
