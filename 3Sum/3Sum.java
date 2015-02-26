9fc00dbb1f6f6f8ec3c80e9e1ba1f70e4e234b36
threeears <zichennie@gmail.com>
2015-02-26 10:56:57 -0800

    SecondRound

--- 3Sum/3Sum.java
+++ 3Sum/3Sum.java
@@ -21,167 +21,64 @@ import java.util.ArrayList;
 
 //arraylist?list?
 public class Solution {
-	public static void main(String[] args){
-		Solution test = new Solution();
-		int[] num={7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
-		//int[] num={0,0,0};
-		System.out.println(test.threeSum3(num));
-	}
-
-	// wrong answer, one element can have muptiple answers
-	public List<List<Integer>> threeSum(int[] num) {
-    	List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Arrays.sort(num);
-
-        int min = 0;
-        int max = num.length-1;
-        int mid = 0;
-
-        while(min+1<max ){
-        		
-        		if(num[min]+num[max]+num[min+1]>0){//smallest + max<0. max--
-        			max--;
-        			continue;
-        		}
-        		else if(num[min]+num[max]+num[max-1]<0){//small+largest<0, small++
-        			min++;
-        			continue;
-        		}
-        		else{// there would be an answer between min and max
-        			mid = min+1;
-        			while(min+1<max && mid<max){
-		        		if(num[min]+num[max]+num[mid]==0)
-		        			//add min mid max in list
-		        		{
-		        			ArrayList<Integer> temp = new ArrayList<Integer>();
-		        			temp.add(num[min]);
-		        			temp.add(num[mid]);
-		        			temp.add(num[max]);
-
-		        			result.add(temp);
-
-		        			//min++;
-		        			max--;
-		        			continue;
-		        		}
-		        		mid++;
-        			}// end while
-        		}// end else   
-        		min++;
-        		max--;
-    	}
-        return result;
-	}
 	
-	//O(n^3), bad complexity
-	public List<List<Integer>> threeSum2(int[] num){
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Arrays.sort(num);
-        int min=0;
-        int max=num.length-1;
-        int mid=0;
-        
-        while(min<=num.length-3 ){
-            max=num.length-1;
-        	while(max-2>=min){
-        		if(num[min]+num[max]+num[min+1]>0){//smallest + max<0. max--
-        		}
-        		else if(num[min]+num[max]+num[max-1]<0){//small+largest<0, small++
-        		}
-        		else{// there would be an answer between min and max
-        			mid = min +1;
-        			while(mid<max){
-		        		if(num[min]+num[max]+num[mid]==0)
-		        			//add min mid max in list
-		        		{
-		        			ArrayList<Integer> temp = new ArrayList<Integer>();
-		        			temp.add(num[min]);
-		        			temp.add(num[mid]);
-		        			temp.add(num[max]);
-		        			
-		        			if(result.contains(temp)==false)
-		        				result.add(temp);
-
-		        			break;
-		        		}
-		        		mid++;
-        			}// end while
-        	}
-        	max--;
-        }
-        	min++;
-        } 
-        
-        return result;
-	}
-
 
 	// O(n^2), because 2sum can be accomplished in O(n), either by hashmap or two pointers
 	
-	public List<List<Integer>> threeSum3(int[] num){
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Arrays.sort(num);
-        int min=0;
-        int max=num.length-1;
-        int mid=0;
-        
-        for(;min<=num.length-3;min++){
-        	int negative = -num[min];
-        	max = num.length-1;
-        	mid = min+1;
-        	if(min!=0 && num[min]==num[min-1]){
-        		continue;
-        	}
-     //   	while(mid<max){
-     //   		if(num[mid]+num[max]>negative){
-     //   			max--;
-     //   		}
-     //   		else if(num[mid]+num[max]<negative){
-     //   			mid++;
-     //   		}
-     //   		else{
-     //   			ArrayList<Integer> temp = new ArrayList<Integer>();
-     //   			temp.add(num[min]);
-     //   			temp.add(num[mid]);
-     //   			temp.add(num[max]);
-       			
-   		// 		result.add(temp);
-
-     //   			while (mid < max && num[mid] == num[mid + 1])
-					// 	mid++;
-
-					// while (mid < max && num[max] == num[max - 1])
-					// 	max--;
-     //   			mid++;
-     //   		}
-     //   	}
-       		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        	while(mid<=max){
-        		
-        		if(map.containsKey(num[mid])){
-        			int k  = map.get(num[mid]);
-        			ArrayList<Integer> temp = new ArrayList<Integer>();
-        			temp.add(num[min]);
-        			temp.add(num[mid]);
-        			temp.add(num[k]);
-        			
-    				result.add(temp);
-
-        			while (mid < max && num[mid] == num[mid + 1])
-						mid++;
- 
-        			mid++;
-        		}
-        		else{
-        			map.put(negative-num[mid], mid);
-        			mid++;
-        		}
-        	}
-        	map.clear();
+	  public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        Set<ArrayList<Integer>> res = new HashSet<ArrayList<Integer>>();
+        if (num == null || num.length <= 2)
+            return new ArrayList<ArrayList<Integer>>(res);
+        Arrays.sort(num); // Sort to avoid duplicates and keep ascending order
+        for (int i = 0; i < num.length; i++){
+            int first = num[i];
+            int j = i+1, k = num.length-1;
+            while (j < k){
+                int sum = first + num[j] + num[k];
+                if (sum == 0){
+                    ArrayList<Integer> r = new ArrayList<Integer>();
+                    r.add(first);
+                    r.add(num[j]);
+                    r.add(num[k]);
+                   // if (!res.contains(r))// aleady using set, no need detect duplicates
+                        res.add(r);
+                    j++;
+                    k--;
+                }else if (sum > 0)  k--;
+                else j++;
+            }
         }
-        return result;
-	}
+        return new ArrayList<ArrayList<Integer>>(res);
+    }
+
+    // more efficient one
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (num==null || num.length<=2)
+            return res;
+        int N = num.length;
+        Arrays.sort(num);
+        for (int k=0; k<N; k++){
+            if (k>0 && num[k]==num[k-1]) // avoid duplicates
+                continue;
+            int i = k+1, j = N-1;
+            while (i<j){
+                if (num[k] + num[i] + num[j] == 0){
+                    ArrayList<Integer> r = new ArrayList<Integer>();
+                    r.add(num[k]); r.add(num[i]); r.add(num[j]);
+                    res.add(r);
+                    // avoid duplicates
+                    do{i++;}while(i<N && num[i]==num[i-1]);
+                    do{j--;}while(j>=0 && num[j]==num[j+1]);
+                }else if (num[k] + num[i] + num[j] < 0)
+                    i++;
+                else
+                    j--;
+            }
+        }
+        return res;
+    }
 }
 // runtime limited problem lies in list.contain(), this function increases complexity a lot!! because
 // we have already sort the array, why not just compare with the next element in the array
+// or use SET to avoid duplication
--- RomanToInteger/Solution.java
+++ RomanToInteger/Solution.java
@@ -8,36 +8,33 @@
 */
 
 public class Solution {
-     public int romanToInt(String s) {
+  public int romanToInt(String s) {
 
-    	HashMap<String, Integer> map = new HashMap<String, Integer>();
-    	map.put("I",1);
-    	map.put("V",5);
-    	map.put("X",10);
-    	map.put("L",50);
-    	map.put("C",100);
-    	map.put("D",500);
-    	map.put("M",1000);
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
 
-    	int i = 0;
-    	int result = 0;
-    	while(i<s.length()){
-    		String rbit = s.charAt(i)+"";
-    		if(i+1<s.length()){
-    			String nxtbit = s.charAt(i+1)+"";
-    			if(map.get(rbit)<map.get(nxtbit)){
-    				result += map.get(nxtbit)-map.get(rbit);
-    				i++;// one step furturer
-    			}
-    			else{
-    				result += map.get(rbit);
-    			}
-    		}
-    		else{
-    			result +=map.get(rbit);
-    		}
-    		i++;
-    	}
-        return result;
+        int i = 0;
+        int res = 0;
+        while(i<s.length()){
+            int this_num = map.get(s.charAt(i));
+            if(i+1<s.length())
+            {
+                int next_num = map.get(s.charAt(i+1));
+                if(this_num<next_num){
+                    res -= this_num;
+                    i++;
+                    continue;
+                }
+            }        
+            res +=this_num;
+            i++;
+        }
+        return res;
     }
 }
